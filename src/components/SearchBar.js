ef3b0d4947f95a1163a140ddf9ce0b084f90cf0e
Eder <chlrobert93@gmail.com>
2021-07-05 16:05:00 -0500

    Recordatorio controladores de eventos

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+   state = { term: 'lkaksjsh'};
+
+   onInpiutChange = () => {
+
+   };
+
     render(){
-        return <div>SearchBar</div>;
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text" value={this.state.term} onChange={this.onInpiutChange} />
+                    </div>
+                </form>
+            </div>
+        );
+            
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-/*
+import "semantic-ui-css/semantic.min.css";
+
 if (module.hot) {
     module.hot.accept();
   }
-*/
+
 
 ReactDOM.render(<App />,document.querySelector('#root'));
